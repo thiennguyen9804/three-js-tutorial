e734ccc03425c99279f374176d6dc42013718edd
hayashing <thiennguyen9804@gmail.com>
2025-05-19 17:27:35 +0700

    sth

--- react-portfolio/src/sections/Hero.tsx
+++ react-portfolio/src/sections/Hero.tsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import { PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import HackerRoom from '../components/HackerRoom'
+import CanvasLoader from '../components/CanvasLoader'
+import { Leva, useControls } from 'leva'
 
 const Hero = () => {
+  const controls = useControls(
+    'HackerRoom',
+    {
+      positionX: {
+        value: 2.5,
+        min: -10,
+        max: 10,
+      },
+      positionY: {
+        value: 2.5,
+        min: -10,
+        max: 10,
+      },
+      positionZ: {
+        value: 2.5,
+        min: -10,
+        max: 10,
+      },
+      rotationX: {
+        value: 0, 
+        min: -10,
+        max: 10
+      },
+      rotationY: {
+        value: 0, 
+        min: -10,
+        max: 10
+      },
+      rotationZ: {
+        value: 0, 
+        min: -10,
+        max: 10
+      },
+      scaleX: {
+        value: 1,
+        min: 0.1,
+        max: 10
+      },
+      scaleY: {
+        value: 1,
+        min: 0.1,
+        max: 10
+      },
+      scaleZ: {
+        value: 1,
+        min: 0.1,
+        max: 10
+      },
+    }
+  );
+
   return (
-    <div>Hero</div>
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi I'am Hayashing
+          <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              position={[2, -8, 2]}
+              rotation={[0, -Math.PI, 0]}
+              scale={[0.1, 0.1, 0.1]}
+              // position={[controls.positionX, controls.positionY, controls.positionZ]}
+              // scale={[controls.scaleX, controls.scaleY, controls.scaleZ]}
+              // rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+
+        </Canvas>
+      </div>
+    </section>
   )
 }
 
